69152fe1792db879187436723a4dffd2a2c37046
Sneha Das <154408198+aishseojudin@users.noreply.github.com>
2024-10-15 16:19:00 +0530

    Update TowerOfHanoi.java

--- TowerOfHanoi.java
+++ TowerOfHanoi.java
@@ -1,16 +1,14 @@
 import java.util.Scanner;
-class TowerOfHanoi{
+
+class TowerOfHanoi {
     public static void main(String[] args) {
-        Scanner sc ;
-        sc = new Scanner(System.in);
-        System.out.print("Enter ring count : ");
-        int height;
-        height = sc.nextInt();
-        Towers t;
-        t = new towers(height);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter ring count: ");
+        int height = sc.nextInt();
+        Towers t = new Towers(height);
         t.printTowers();
-        TowerSolver solver;
-        solver = new TowerSolver(t);
-        solver.solve(height,1,3,2);
+        TowerSolver solver = new TowerSolver(t);
+        solver.solve(height, 1, 3, 2);
+        sc.close();
     }
 }
